7209fae0459f129b878f2465479d758f39333cef
Ludin-Najera <69707485+Ludin-Najera@users.noreply.github.com>
2020-09-23 17:34:26 -0600

    update

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('seguridad/login', 'Seguridad\LoginController@index')->name('login');
+Route::get('', 'AdminController@index');
+Route::group(['middleware' => 'auth'], function (){
+    
+    Route::get('', 'AdminController@index');
+
+});
